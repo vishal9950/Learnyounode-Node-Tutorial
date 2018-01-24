b1c51534443f444652122d5a9d1d57cdaa2b0390
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-24 12:40:38 +0530

    Ex1: #TC2: console.log called with a number

--- Ex1/1_hello.test.js
+++ Ex1/1_hello.test.js
@@ -1,9 +1,15 @@
 const logger = require('./1_hello');
 
 describe('Functionality of console.log: ', () => {
-  test('Should call console.log', () => {
+  test('Should call console.log for a string: ', () => {
     global.console.log = jest.fn();
     logger('HELLO WORLD');
     expect(global.console.log).toHaveBeenCalledWith('HELLO WORLD');
+  });
+
+  test('Should call console.log a number: ', () => {
+    global.console.log = jest.fn();
+    logger(1234);
+    expect(global.console.log).toHaveBeenCalledWith(1234);
   });
 });
