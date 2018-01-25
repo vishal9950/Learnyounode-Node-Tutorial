c7337e0f0df070bebbbe989c63f66eb890c62214
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-25 17:54:41 +0530

    Ex3: #TC4: get new lines in test2.txt

--- Ex3/3_firstIO.test.js
+++ Ex3/3_firstIO.test.js
@@ -14,4 +14,8 @@ describe('Test functionality of calcNewLine: ', () => {
   test('Should work for a file with two new lines: ', () => {
     expect(calcNewLine('./Ex3/test1.txt')).toBe(2);
   });
+
+  test('Should work for a file with no lines: ', () => {
+    expect(calcNewLine('./Ex3/test2.txt')).toBe(0);
+  });
 });
