4345d4dcdfb005512311bb9715161e0c74c6a3bf
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-27 22:30:19 +0530

    Ex5: #TC2: undefined args test

--- Ex5/5_filtered.js
+++ Ex5/5_filtered.js
@@ -1,6 +1,6 @@
 const validateInput = (input) => {
-  if (input === null) {
-    console.log('Err1: null args provided!');
+  if ((input === null) || (input === undefined)) {
+    console.log(`Err1: ${input} args provided!`);
     return false;
   }
   return true;
--- Ex5/5_filtered.test.js
+++ Ex5/5_filtered.test.js
@@ -4,4 +4,8 @@ describe('Arguments Validation: ', () => {
   test('Should not work for null args: ', () => {
     expect(filtered(null)).toBe(false);
   });
+
+  test('Should not work for undefined args: ', () => {
+    expect(filtered(undefined)).toBe(false);
+  });
 });
