eed5a5dcff36d2a0d097112ab520aa65b5f1085b
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-28 13:56:09 +0530

    Ex6: #TC2: undefined args

--- Ex6/6_module.test.js
+++ Ex6/6_module.test.js
@@ -4,4 +4,8 @@ describe('Argument Validation: ', () => {
   test('Should not work for null args: ', () => {
     expect(filesLS(null)).toBe(false);
   });
+
+  test('Should not work for undefined args: ', () => {
+    expect(filesLS(undefined)).toBe(false);
+  });
 });
