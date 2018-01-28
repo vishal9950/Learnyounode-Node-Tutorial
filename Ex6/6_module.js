ee14233d609336471aeec396f36c8d257d27e77f
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-28 16:46:16 +0530

    Ex6: Verified

--- Ex6/6_module.js
+++ Ex6/6_module.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const validateInput = (input) => {
   if ((input === null) || (input === undefined)) {
@@ -16,13 +17,36 @@ const pathExists = path => fs.exists(path, (exists) => {
   return false;
 });
 
-const filesLS = (filePath) => {
-  if (!validateInput(filePath)) {
+const filesLS = (filePath, extension) => {
+  const ext = `.${extension}`;
+  // console.log(ext);
+  if ((!validateInput(filePath)) && (!validateInput(extension))) {
+    // console.log(1);
     return false;
   }
   if (!pathExists(filePath)) {
+    console.log(2);
     return false;
   }
+
+  fs.readdir(filePath, (err, files) => {
+    // const result = [];
+    if (err) {
+      console.log(err);
+      return false;
+    }
+    for (let i = 0; i < files.length; i += 1) {
+      if (path.extname(files[i]) === ext) {
+        console.log(files[i]);
+        // result.push(files[i]);
+      }
+    }
+    return true;
+    // for (let i = 0; i < result.length; i += 1) {
+    //   console.log(result[i]);
+    // }
+    // return result;
+  });
   return true;
 };
 
---
+++ Ex6/6_module1.js
@@ -0,0 +1,3 @@
+const filesLS = require('./6_module');
+
+filesLS(process.argv[2], process.argv[3]);
