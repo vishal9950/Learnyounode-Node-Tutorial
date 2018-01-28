6b3c70e8bd153a9155dd0660be628093f04e57dd
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-28 23:32:38 +0530

    Ex7: Verified

---
+++ Ex7/7_http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.get(process.argv[2], (response) => {
+  response.setEncoding('utf8');
+  response.on('data', (data) => {
+    console.log(data);
+  });
+});
+
+module.exports = http.get;
