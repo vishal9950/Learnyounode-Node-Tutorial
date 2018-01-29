86740391401b4aa6b5751908d9ea658db68fd944
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-30 02:09:43 +0530

    Ex7: #TC1: data checked for successful get req to http

--- Ex7/7_http.js
+++ Ex7/7_http.js
@@ -1,10 +1,14 @@
 const http = require('http');
 
-http.get(process.argv[2], (response) => {
-  response.setEncoding('utf8');
-  response.on('data', (data) => {
-    console.log(data);
+const asyncHTTP = (url, callback) => {
+  http.get(url, (response) => {
+    console.log(process.argv[2]);
+    response.setEncoding('utf8');
+    response.on('data', (data) => {
+      console.log(data);
+      callback('data stream');
+    });
   });
-});
+};
 
-module.exports = http.get;
+module.exports = asyncHTTP;
---
+++ Ex7/7_http.test.js
@@ -0,0 +1,11 @@
+const asyncHTTP = require('./7_http');
+
+describe('Funcitonality of http.get: ', () => {
+  test('Verify data for successful get request: ', (done) => {
+    const callback = (data) => {
+      expect(data).toBe('data stream');
+      done();
+    };
+    asyncHTTP('http://www.google.com', callback);
+  });
+});
